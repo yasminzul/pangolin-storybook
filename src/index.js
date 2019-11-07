cde5b9b069aef9fdd0fe64dd19b0b4e046469a92
yasminzul <ainayasminmpm@gmail.com>
2019-11-07 11:30:06 +0800

    setting up loaders

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import css from './css/style.css';
+var $ = require("jquery");
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'world'], ' ');
 
   return element;
 }
