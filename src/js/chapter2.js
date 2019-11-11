425a306102cd1ceb88511166b6c7d69b39f12131
richasyal <richasyal@gmail.com>
2019-11-11 15:25:03 +0800

    fadeout

--- src/js/chapter2.js
+++ src/js/chapter2.js
@@ -36,4 +36,72 @@ $(function () {
 			.setTween(slides)
 			.addIndicators()
 			.addTo(controller);
+
+
+// fadein
+		var t = TweenLite.from(".row", 1,{
+		  autoAlpha:1,
+		  paused:true, ease:Linear.easeNone
+		}),
+		    scrollThreshold = {"start":0, "stop":600};
+
+		$(window).on("scroll",function(e){
+		  
+		  var currentScroll = $(window).scrollTop(),
+		      currentProgress = ( currentScroll - scrollThreshold.stop ) / ( scrollThreshold.stop - scrollThreshold.start );
+		  
+		  if(currentScroll > scrollThreshold.start && currentScroll < scrollThreshold.stop){
+		    t.progress(currentProgress);
+		  }else if( currentScroll > scrollThreshold.start ){
+		    t.progress(0);
+		  }else if( currentScroll > scrollThreshold.stop){
+		    t.progress(0);
+		  }
+		  
+		});
+
 	});
+
+
+
+// //Run Function when DOM is ready. 
+// $(function() {
+// 	//Initialize Controller
+// 	var scrollMagicController = new ScrollMagic.Controller({loglevel: 3});
+
+// 	//Create a fadein animation for 1.5s
+// 	var fadein_tween = TweenMax.to('#fadein-trigger > div', 1.0, {
+// 		opacity: 1
+// 	});
+// 	//Create a fadeout animation for 1.5s on the element with id = "fadein-trigger"
+// 	var fadeout_tween = TweenMax.to('#fadein-trigger > div', 1.0, {
+// 		opacity: 0
+// 	});
+// 	//Create a new ScrollMagic Scene
+// 	var fadein_scene = new ScrollMagic.Scene({
+// 		triggerElement: '#fadein-trigger',
+// 		reverse: true
+// 	})
+// 	.setTween(fadein_tween)
+// 	.addIndicators({name: "1 (duration: 1.0 seconds)"})
+// 	.addTo(scrollMagicController);
+// 	//Create a second ScrollMagic Scene
+// 	var fadeout_scene = new ScrollMagic.Scene({
+// 		triggerElement: '#fadeout-trigger',
+// 		reverse: true
+// 	})
+// 	.setTween(fadeout_tween)
+// 	.addIndicators({name: "2 (duration: 1.0 seconds)"})
+// 	.addTo(scrollMagicController);
+
+// 	//Add debug fixed indicators
+// 	//fadein_scene.addIndicators();
+//   //fadeout_scene.addIndicators();
+// });
+
+
+// text fadeout
+
+// $(window).scroll(function(){
+//     $(".row").css("opacity", 1 - $(window).scrollTop() / 250);
+//   });
