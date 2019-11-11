f8052e3846ac6784c7f629c16046c657a0e4e99b
yasminzul <ainayasminmpm@gmail.com>
2019-11-11 11:17:48 +0800

    finished merging from master

--- src/js/chapter2.js
+++ src/js/chapter2.js
@@ -7,3 +7,31 @@ import TimelineMax from 'TimelineMax';
 import ScrollMagic from 'ScrollMagic';
 import 'animation.gsap';
 import 'debug.addIndicators';
+
+$(function () {
+		var controller = new ScrollMagic.Controller();
+		var slides = new TimelineMax()
+
+			.to("#slideContainer", 0.5, {z: -150})
+			.to("#slideContainer", 1,   {x: "-25%"})
+			.to("#slideContainer", 0.5, {z: 0})
+
+			.to("#slideContainer", 0.5, {z: -150, delay: 1})
+			.to("#slideContainer", 1,   {x: "-50%"})
+			.to("#slideContainer", 0.5, {z: 0})
+
+			.to("#slideContainer", 0.5, {z: -150, delay: 1})
+			.to("#slideContainer", 1,   {x: "-75%"})
+			.to("#slideContainer", 0.5, {z: 0})
+
+
+		var scene = new ScrollMagic.Scene({
+				triggerElement: "#pinContainer",
+				triggerHook: "onLeave",
+				duration: "400%"
+			})
+			.setPin("#pinContainer")
+			.setTween(slides)
+			.addIndicators()
+			.addTo(controller);
+	});
