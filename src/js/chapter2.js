6bd2f38187a948b0035ce36e1b2bb9c9833efad7
richasyal <richasyal@gmail.com>
2019-11-11 12:16:26 +0800

    page 1 desktop done

--- src/js/chapter2.js
+++ src/js/chapter2.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import '../css/font.css'
 import css from '../css/chapter2.css';
+import '../css/chapter.css'
 var $ = require("jquery");
 
 import TweenMax from 'TweenMax';
