6870905a00cbf7118ad0c2346c1d6b1381da6a4e
yasminzul <ainayasminmpm@gmail.com>
2019-11-11 19:00:11 +0800

    burger menu done

--- src/js/view.js
+++ src/js/view.js
@@ -1,3 +1,12 @@
 import _ from 'lodash';
 import css from '../css/view.css';
 var $ = require("jquery");
+
+
+
+
+$('#burger-icon').on('click', function(e){
+  $(this).toggleClass('open-icon');
+  $('.menu').toggleClass('open-menu');
+  console.log('toggling');
+});
