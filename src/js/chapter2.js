9b29764356654b12bd98b60b0c3c800b8cd2a5b8
yasminzul <ainayasminmpm@gmail.com>
2019-11-11 11:08:43 +0800

    added ScrollMagic

--- src/js/chapter2.js
+++ src/js/chapter2.js
@@ -2,8 +2,8 @@ import _ from 'lodash';
 import css from '../css/chapter2.css';
 var $ = require("jquery");
 
-import * as ScrollMagic from "scrollmagic"; // Or use scrollmagic-with-ssr to avoid server rendering problems
-import { TweenMax, TimelineMax } from "gsap"; // Also works with TweenLite and TimelineLite
-import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
-
-ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
+import TweenMax from 'TweenMax';
+import TimelineMax from 'TimelineMax';
+import ScrollMagic from 'ScrollMagic';
+import 'animation.gsap';
+import 'debug.addIndicators';
