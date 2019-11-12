1921ff7f24971eeac4b60b9ee8c75c6a9987e117
yasminzul <ainayasminmpm@gmail.com>
2019-11-12 16:28:05 +0800

    Update chapter2.js

--- src/js/chapter2.js
+++ src/js/chapter2.js
@@ -3,8 +3,8 @@ import '../css/font.css'
 import css from '../css/chapter2.css';
 var $ = require("jquery");
 
-import TweenMax from 'TweenMax';
-import TimelineMax from 'TimelineMax';
-import ScrollMagic from 'ScrollMagic';
-import 'animation.gsap';
-import 'debug.addIndicators';
+// import TweenMax from 'TweenMax';
+// import TimelineMax from 'TimelineMax';
+// import ScrollMagic from 'ScrollMagic';
+// import 'animation.gsap';
+// import 'debug.addIndicators';
