b55f957c876fc23ef9f7a6dd7fb290beeee4aaf5
yasminzul <ainayasminmpm@gmail.com>
2019-11-13 14:17:08 +0800

    Update view.js

--- src/js/view.js
+++ src/js/view.js
@@ -18,25 +18,25 @@ $( window ).on( "load", function() {
     $('.menu').toggleClass('open-menu');
   });
 
-  $(".animsition").animsition({
-    inClass: 'fade-in-up',
-    outClass: 'fade-out-up',
-    inDuration: 1500,
-    outDuration: 800,
-    loading: true,
-    loadingParentElement: 'body', //animsition wrapper element
-    loadingClass: 'animsition-loading',
-    loadingInner: '', // e.g '<img src="loading.svg" />'
-    timeout: false,
-    timeoutCountdown: 5000,
-    onLoadEvent: true,
-    browser: [ 'animation-duration', '-webkit-animation-duration'],
-    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-    overlay : false,
-    overlayClass : 'animsition-overlay-slide',
-    overlayParentElement : 'body',
-    transition: function(url){ window.location.href = url; }
-  });
-
 })
+
+$(".animsition").animsition({
+  inClass: 'fade-in-up',
+  outClass: 'fade-out-up',
+  inDuration: 1500,
+  outDuration: 800,
+  loading: true,
+  loadingParentElement: 'body', //animsition wrapper element
+  loadingClass: 'animsition-loading',
+  loadingInner: '', // e.g '<img src="loading.svg" />'
+  timeout: false,
+  timeoutCountdown: 5000,
+  onLoadEvent: true,
+  browser: [ 'animation-duration', '-webkit-animation-duration'],
+  // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+  // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+  overlay : false,
+  overlayClass : 'animsition-overlay-slide',
+  overlayParentElement : 'body',
+  transition: function(url){ window.location.href = url; }
+});
