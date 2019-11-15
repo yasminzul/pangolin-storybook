7530663e1d25104001b5d0d7ec146c548be999f5
richasyal <richasyal@gmail.com>
2019-11-15 14:59:22 +0800

    jquery for popups working

--- src/js/chapter12.js
+++ src/js/chapter12.js
@@ -13,14 +13,44 @@ import Swiper from './../js/plugin/swiper.min.js';
       },
     });
 
+// lactation 
 
 $("#lactation-btn").on("click", function() {
   $("#lactation-popup").addClass("active");
-  console.log('adding active');
 });
-
 
 $("#lactation-close").on("click", function() {
   $("#lactation-popup").removeClass("active");
-  console.log('remove class');
+});
+
+// furuncle
+
+$("#furuncle-btn").on("click", function() {
+  $("#furuncle-popup").addClass("active");
+});
+
+
+$("#furuncle-close").on("click", function() {
+  $("#furuncle-popup").removeClass("active");
+});
+
+// arthritis
+
+$("#arthritis-btn").on("click", function() {
+  $("#arthritis-popup").addClass("active");
+});
+
+
+$("#arthritis-close").on("click", function() {
+  $("#arthritis-popup").removeClass("active");
+});
+
+// detox
+
+$("#detox-btn").on("click", function() {
+  $("#detox-popup").addClass("active");
+});
+
+$("#detox-close").on("click", function() {
+  $("#detox-popup").removeClass("active");
 });
