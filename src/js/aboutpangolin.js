7abf06c2668769618cc8c0ecfb18a832d71ca018
yasminzul <ainayasminmpm@gmail.com>
2019-11-15 16:52:11 +0800

    Update aboutpangolin.js

--- src/js/aboutpangolin.js
+++ src/js/aboutpangolin.js
@@ -6,14 +6,6 @@ import '../css/swiper.min.css';
 import Swiper from './../js/plugin/swiper.min.js';
 
 
-  var swiper = new Swiper('.swiper-container', {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-
-
 //appends an "active" class to .popup and .popup-content when the "Open" button is clicked
 $("#orangasli-btn").on("click", function() {
   $("#orangasli-popup").css('display','block');
