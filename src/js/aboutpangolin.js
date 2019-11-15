3c99e51a4e0bf1b1c19e401f025a21cbd5399f66
yasminzul <ainayasminmpm@gmail.com>
2019-11-15 16:48:25 +0800

    fixed popup

--- src/js/aboutpangolin.js
+++ src/js/aboutpangolin.js
@@ -16,12 +16,17 @@ import Swiper from './../js/plugin/swiper.min.js';
 
 //appends an "active" class to .popup and .popup-content when the "Open" button is clicked
 $("#orangasli-btn").on("click", function() {
-  $("#orangasli-popup").addClass("active");
-  console.log('adding active');
+  $("#orangasli-popup").css('display','block');
+  var swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
 });
 
-//removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked
 $(".close").on("click", function() {
-  $("#orangasli-popup").removeClass("active");
-  console.log('remove class');
+  $("#orangasli-popup").css('display','none');
 });
