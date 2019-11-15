7ad0ac77452d250edafb544d44f1233aad2e4db0
yasminzul <ainayasminmpm@gmail.com>
2019-11-15 10:40:37 +0800

    setting up chapter 12

--- src/js/chapter12.js
+++ src/js/chapter12.js
@@ -1,3 +1,14 @@
 import _ from 'lodash';
 import css from '../css/chapter12.css';
 var $ = require("jquery");
+
+import '../css/swiper.min.css';
+import Swiper from './../js/plugin/swiper.min.js';
+
+
+  var swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
