a06f3e318e3fb367dc4657e71a9b354487cd6cc0
richasyal <richasyal@gmail.com>
2019-11-18 16:36:20 +0800

    setting up map structure

--- src/js/chapter8.js
+++ src/js/chapter8.js
@@ -1,3 +1,25 @@
 import _ from 'lodash';
-import css from '../css/chapter8.css';
+import css from '../css/chapter12.css';
 var $ = require("jquery");
+
+import '../css/swiper.min.css';
+import Swiper from './../js/plugin/swiper.min.js';
+
+
+// //appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+$("#lactation-btn, #furuncle-btn, #arthritis-btn, #detox-btn").on("click", function() {
+  $(this).next().css('display','block');
+  var swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+});
+
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked
+$(".close").on("click", function() {
+  $(this).closest('.popup').css('display','none');
+});
+
