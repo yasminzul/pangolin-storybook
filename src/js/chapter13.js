baeca4f8d2e178c65b8bf87ce4162a7dc2bdb214
richasyal <richasyal@gmail.com>
2019-11-25 17:40:52 +0800

    ch 13 slider

--- src/js/chapter13.js
+++ src/js/chapter13.js
@@ -2,4 +2,14 @@ import _ from 'lodash';
 import '../css/font.css';
 import css from '../css/chapter13.css';
 import '../css/chapter.css';
+import '../css/swiper.min.css';
+import Swiper from './../js/plugin/swiper.min.js';
 var $ = require("jquery");
+
+
+var swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+ });
--- src/js/chapter2.js
+++ src/js/chapter2.js
@@ -2,6 +2,8 @@ import _ from 'lodash';
 import '../css/font.css';
 import css from '../css/chapter2.css';
 import '../css/chapter.css';
+import '../css/swiper.min.css';
+import Swiper from './../js/plugin/swiper.min.js';
 var $ = require("jquery");
 
 $(document).ready(function(){
@@ -12,3 +14,10 @@ $(document).ready(function(){
         $(targetBox).show();
     });
 });
+
+var swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+ });
