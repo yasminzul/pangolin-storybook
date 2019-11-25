201a0f83758cf19ac8929b3ef3d4291c5f6a09ca
richasyal <richasyal@gmail.com>
2019-11-25 12:07:41 +0800

    ch 3 soundcites

--- src/js/chapter3.js
+++ src/js/chapter3.js
@@ -2,4 +2,5 @@ import _ from 'lodash';
 import '../css/font.css';
 import css from '../css/chapter3.css';
 import '../css/chapter.css';
+import './../js/plugin/soundcite.min.js'; 
 var $ = require("jquery");
