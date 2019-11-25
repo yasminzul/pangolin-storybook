7f3aee6a5b0ddcbe90210a3a858609d90fa3c172
richasyal <richasyal@gmail.com>
2019-11-25 16:34:54 +0800

    change slider to swiper

--- src/js/chapter9.js
+++ src/js/chapter9.js
@@ -2,15 +2,15 @@ import _ from 'lodash';
 import '../css/font.css';
 import css from '../css/chapter9.css';
 import '../css/chapter.css';
+import '../css/swiper.min.css';
+import Swiper from './../js/plugin/swiper.min.js';
 var $ = require("jquery");
 
 
-$(document).ready(function(){
-    $('input[type="radio"]').click(function(){
-    	console.log('click');
-        var inputValue = $(this).attr("value");
-        var targetBox = $("." + inputValue);
-        $(".caption").not(targetBox).hide();
-        $(targetBox).show();
+  var swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
     });
-});
+
