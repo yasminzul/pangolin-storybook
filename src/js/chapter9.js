74683719edc57fc4a9a98e73556df78e29156539
richasyal <richasyal@gmail.com>
2019-11-25 16:24:16 +0800

    updates

--- src/js/chapter9.js
+++ src/js/chapter9.js
@@ -3,3 +3,14 @@ import '../css/font.css';
 import css from '../css/chapter9.css';
 import '../css/chapter.css';
 var $ = require("jquery");
+
+
+$(document).ready(function(){
+    $('input[type="radio"]').click(function(){
+    	console.log('click');
+        var inputValue = $(this).attr("value");
+        var targetBox = $("." + inputValue);
+        $(".caption").not(targetBox).hide();
+        $(targetBox).show();
+    });
+});
