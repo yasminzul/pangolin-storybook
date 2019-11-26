2e58dd4bfc3d4479afdfcdd4483c479cd577233d
richasyal <richasyal@gmail.com>
2019-11-26 14:48:49 +0800

    updates

--- src/js/chapter5.js
+++ src/js/chapter5.js
@@ -1,4 +1,18 @@
 import _ from 'lodash';
 import '../css/font.css';
 import css from '../css/chapter5.css';
+import '../css/chapter.css';
+import '../css/swiper.min.css';
+import Swiper from './../js/plugin/swiper.min.js';
 var $ = require("jquery");
+
+
+$("#open-trail").on("click", function() {
+  $('.popup').css('display','block');
+  var swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+});
