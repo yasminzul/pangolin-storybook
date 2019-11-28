1aab3776406d2f8b9bac038d93ecd62377b3b8fd
Yasmin Zulhaime <32533400+yasminzul@users.noreply.github.com>
2019-11-28 13:26:00 +0800

    swiper for pictures in every chapter adjusted

--- src/js/chapter9.js
+++ src/js/chapter9.js
@@ -1,16 +1,15 @@
 import _ from 'lodash';
 import '../css/font.css';
 import css from '../css/chapter9.css';
-import '../css/chapter.css';
+
 import '../css/swiper.min.css';
 import Swiper from './../js/plugin/swiper.min.js';
 var $ = require("jquery");
 
 
   var swiper = new Swiper('.swiper-container', {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
       },
     });
-
