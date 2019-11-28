2ca9fe31adbacbff59c0289b2e73478b23ef45d5
Yasmin Zulhaime <32533400+yasminzul@users.noreply.github.com>
2019-11-28 20:17:50 +0800

    burgermenu in progress

--- src/js/view.js
+++ src/js/view.js
@@ -7,7 +7,40 @@ import './../js/plugin/swiper.min.js';
 
 var $ = require("jquery");
 
-let menu = "<div id=burger-icon><span></span><span></span><span></span><span></span></div><ul class=menu><a href=index.html><li>Home</li></a><a href=about-pangolin.html><li>The Pangolin</li></a><li>All chapters<ul class=menu-chapter><a href=chapter-1.html><li>Chapter 1 : The Middleman</li></a><a href=chapter-2.html><li>Chapter 2 : The Boatman</li></a><a href=chapter-3.html><li>Chapter 3 : The Smuggler</li></a><a href=chapter-4.html><li>Chapter 4 : The Policeman</li></a><a href=chapter-5.html><li>Chapter 5 : Follow The Trail Of A Smuggled Pangolin in Malaysia</li></a><a href=chapter-6.html><li>Chapter 6 : The Waiting Wife</li></a><a href=chapter-7.html><li>Chapter 7 : The Towkay</li></a><a href=chapter-8.html><li>Chapter 8 : The Secret Pangolin Smuggling Routes Around The World</li></a><a href=chapter-9.html><li>Chapter 9 : The Social Media Star</li></a><a href=chapter-10.html><li>Chapter 10 : The Authorities</li></a><a href=chapter-11.html><li>Chapter 11 : The Two Doctors</li></a><a href=chapter-12.html><li>Chapter 12 : The No-Pangolin Recipes</li></a><a href=chapter-13.html><li>Chapter 13 : The Hunters</li></a><a href=chapter-14.html><li>Chapter 14 : Rainie’s Song</li></a><a href=chapter-15.html><li>Chapter 15 : The Forest</li></a></ul></li><a href=quiz.html><li>Quiz: Which Pangolin Protector Are You?</li></a><a href=about-project.html><li>About this project</li></a><a href=https://www.rage.com.my/><li>The Pangolin Reports: Trafficked to Extenction</li></a><li>Share<ul class=menu-share><a href=https://www.rage.com.my/ target=_blank><li><i class=ion-logo-facebook></i></li></a><a href=https://www.rage.com.my/ target=_blank><li><i class=ion-logo-twitter></i></li></a><a href=https://www.rage.com.my/ target=_blank><li><i class=ion-md-link></i></li></a></ul></li><li><ul class=menu-footer><a href=https://www.rage.com.my/ target=_blank><li>R.AGE</li></a><li>© 2019 R.AGE</li><a href=http://facebook.com target=_blank><li><i class=ion-logo-facebook></i></li></a><a href=http://twitter.com target=_blank><li><i class=ion-logo-twitter></i></li></a><a href=http://instagram.com target=_blank><li><i class=ion-logo-instagram></i></li></a></ul></li></ul>";
+let menu = `<div id=burger-icon><span></span><span></span><span></span><span></span></div>
+<div class=menu>
+<table>
+<tr>
+  <a href=index.html><td><img src=../gfx/menu/home@3x.png></td></a>
+  <a href=about-pangolin.html><td><img src=../gfx/menu/aboutpangolin@3x.png></td></a>
+  <a href=chapter-1.html><td><img src=../gfx/menu/middleman@3x.png></td></a>
+  <a href=chapter-2.html><td><img src=../gfx/menu/boatman@3x.png></td></a>
+  <a href=chapter-3.html><td><img src=../gfx/menu/smuggler@3x.png></td></a>
+</tr>
+<tr>
+  <a href=chapter-4.html><td><img src=../gfx/menu/policeman@3x.png></td></a>
+  <a href=chapter-5.html><td><img src=../gfx/menu/followtrail@3x.png></td></a>
+  <a href=chapter-6.html><td><img src=../gfx/menu/waitingwife@3x.png></td></a>
+  <a href=chapter-8.html><td><img src=../gfx/menu/routesmap@3x.png></td></a>
+  <a href=chapter-9.html><td><img src=../gfx/menu/socialmedia@3x.png></td></a>
+</tr>
+<tr>
+  <a href=chapter-10.html><td></td></a>
+  <a href=chapter-11.html><td></td></a>
+  <a href=chapter-12.html><td></td></a>
+  <a href=chapter-13.html><td></td></a>
+  <a href=chapter-14.html><td></td></a>
+</tr>
+<tr>
+  <a href=chapter-15.html><td></td></a>
+  <a href=quiz.html><td></td></a>
+  <a href=about-project.html><td></td></a>
+  <a href=https://www.rage.com.my/><td></td></a>
+  <td></td>
+</tr>
+</table>
+  <ul class=menu-footer><a href=https://www.rage.com.my/ target=_blank><li>R.AGE</li></a><li>© 2019 R.AGE</li><a href=http://facebook.com target=_blank><li><i class=ion-logo-facebook></i></li></a><a href=http://twitter.com target=_blank><li><i class=ion-logo-twitter></i></li></a><a href=http://instagram.com target=_blank><li><i class=ion-logo-instagram></i></li></a></ul></li>
+</div>`;
 
 
 let orientation = "<div class=full-orientation></div>";
