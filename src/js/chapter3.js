6941c29bd5748a568cf205e130ca1b9319ec22fd
Yasmin Zulhaime <32533400+yasminzul@users.noreply.github.com>
2019-11-28 17:33:48 +0800

    updating fonts

--- src/js/chapter3.js
+++ src/js/chapter3.js
@@ -2,5 +2,7 @@ import _ from 'lodash';
 import '../css/font.css';
 import css from '../css/chapter3.css';
 import '../css/chapter.css';
-import './../js/plugin/soundcite.min.js'; 
+import '../css/soundcite.min.css';
+
+import './../js/plugin/soundcite.min.js';
 var $ = require("jquery");
--- src/js/chapter4.js
+++ src/js/chapter4.js
@@ -2,7 +2,8 @@ import _ from 'lodash';
 import '../css/font.css';
 import css from '../css/chapter4.css';
 import '../css/chapter.css';
-import './../js/plugin/soundcite.min.js'; 
+import '../css/soundcite.min.css';
+import './../js/plugin/soundcite.min.js';
 var $ = require("jquery");
 
 import '../css/swiper.min.css';
