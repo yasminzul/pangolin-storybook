fef514752cbe8a7db900b7c5838e52de9568e18b
richasyal <richasyal@gmail.com>
2019-12-01 13:22:43 +0800

    list changes

--- src/js/chapter3.js
+++ src/js/chapter3.js
@@ -2,7 +2,18 @@ import _ from 'lodash';
 import '../css/font.css';
 import css from '../css/chapter3.css';
 import '../css/chapter.css';
+import '../css/swiper.min.css';
+import Swiper from './../js/plugin/swiper.min.js';
 import '../css/soundcite.min.css';
+
 
 import './../js/plugin/soundcite.min.js';
 var $ = require("jquery");
+
+
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+ });
