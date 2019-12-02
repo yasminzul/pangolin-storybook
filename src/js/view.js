9e171874f49e2a5031d5a2df3c48b1c08dc713d1
richasyal <richasyal@gmail.com>
2019-12-02 11:56:58 +0800

    updates

--- src/js/view.js
+++ src/js/view.js
@@ -11,32 +11,32 @@ let menu = `<div id=burger-icon><span></span><span></span><span></span><span></s
 <div class=menu>
 <table>
 <tr>
-  <td><a href=index.html><img src=home@3x.png></a></td>
-  <td><a href=about-pangolin.html><img src=aboutpangolin@3x.png></a></td>
-  <td><a href=chapter-1.html><img src=middleman@3x.png></a></td>
-  <td><a href=chapter-2.html><img src=boatman@3x.png></a></td>
-  <td><a href=chapter-3.html><img src=smuggler@3x.png></a></td>
+  <td><a href=index.html><img src=/menu/home@3x.png></a></td>
+  <td><a href=about-pangolin.html><img src=/menu/aboutpangolin@3x.png></a></td>
+  <td><a href=chapter-1.html><img src=/menu/middleman@3x.png></a></td>
+  <td><a href=chapter-2.html><img src=/menu/boatman@3x.png></a></td>
+  <td><a href=chapter-3.html><img src=/menu/smuggler@3x.png></a></td>
 </tr>
 <tr>
-  <td><a href=chapter-4.html><img src=policeman@3x.png></a></td>
-  <td><a href=chapter-5.html><img src=followtrail@3x.png></a></td>
-  <td><a href=chapter-6.html><img src=waitingwife@3x.png></a></td>
-  <td><a href=chapter-8.html><img src=routesmap@3x.png></a></td>
-  <td><a href=chapter-9.html><img src=socialmedia@3x.png></a></td>
+  <td><a href=chapter-4.html><img src=/menu/policeman@3x.png></a></td>
+  <td><a href=chapter-5.html><img src=/menu/followtrail@3x.png></a></td>
+  <td><a href=chapter-6.html><img src=/menu/waitingwife@3x.png></a></td>
+  <td><a href=chapter-8.html><img src=/menu/routesmap@3x.png></a></td>
+  <td><a href=chapter-9.html><img src=/menu/socialmedia@3x.png></a></td>
 </tr>
 <tr>
-  <td><a href=chapter-10.html><img src=authorities@3x.png></a></td>
-  <td><a href=chapter-11.html><img src=twodoctors@3x.png></a></td>
-  <td><a href=chapter-12.html><img src=recipe@3x.png></a></td>
-  <td><a href=chapter-13.html><img src=hunters@3x.png></a></td>
-  <td><a href=chapter-14.html><img src=rainies@3x.png></a></td>
+  <td><a href=chapter-10.html><img src=/menu/authorities@3x.png></a></td>
+  <td><a href=chapter-11.html><img src=/menu/twodoctors@3x.png></a></td>
+  <td><a href=chapter-12.html><img src=/menu/recipe@3x.png></a></td>
+  <td><a href=chapter-13.html><img src=/menu/hunters@3x.png></a></td>
+  <td><a href=chapter-14.html><img src=/menu/rainies@3x.png></a></td>
 </tr>
 <tr>
-  <td><a href=chapter-15.html><img src=forest@3x.png></a></td>
-  <td><a href=about-project.html><img src=aboutproject@3x.png></a></td>
-  <td><a href=quiz.html><img src=quiz@3x.png></a></td>
-  <td><a href=https://globalstory.pangolinreports.com/><img src=globalstory@3x.png></a></td>
-  <td><img src=share@3x.png></td>
+  <td><a href=chapter-15.html><img src=/menu/forest@3x.png></a></td>
+  <td><a href=about-project.html><img src=/menu/aboutproject@3x.png></a></td>
+  <td><a href=quiz.html><img src=/menu/quiz@3x.png></a></td>
+  <td><a href=https://globalstory.pangolinreports.com/><img src=/menu/globalstory@3x.png></a></td>
+  <td><img src=/menu/share@3x.png></td>
 </tr>
 </table>
   <ul class=menu-footer><a href=https://www.rage.com.my/ target=_blank><li>R.AGE</li></a><li>© 2019 R.AGE</li><a href=http://facebook.com target=_blank><li><i class=ion-logo-facebook></i></li></a><a href=http://twitter.com target=_blank><li><i class=ion-logo-twitter></i></li></a><a href=http://instagram.com target=_blank><li><i class=ion-logo-instagram></i></li></a></ul></li>
