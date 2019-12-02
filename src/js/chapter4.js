a1b33ff63bc7050a166f10d6be05e3d8ecc5ad59
richasyal <richasyal@gmail.com>
2019-12-02 16:50:50 +0800

    pulsing

--- src/js/chapter4.js
+++ src/js/chapter4.js
@@ -20,25 +20,25 @@ $(".close").on("click", function(e) {
   $(this).closest('.popup').css('display','none');
 });
 
-$('#one').on('click', function (){
-	$('.route-legend span').html("<strong>SEPT 2012:</strong><br> Police officer Mohammad Norazzuan Ahmad Zahari arrested at an unspecified location in Kedah, in possession of 18 live pangolins.");
-});
+// $('#one').on('click', function (){
+// 	$('.route-legend span').html("<strong>SEPT 2012:</strong><br> Police officer Mohammad Norazzuan Ahmad Zahari arrested at an unspecified location in Kedah, in possession of 18 live pangolins.");
+// });
 
-$('#two').on('click', function (){
-	$('.route-legend span').html("<strong>NOV 2014:</strong><br> Police officer Mohammad Norazzuan Ahmad Zahari arrested again at the Bukit Kayu Hitam border checkpoint; 43 live pangolins found in the boot of his car.");
-});
+// $('#two').on('click', function (){
+// 	$('.route-legend span').html("<strong>NOV 2014:</strong><br> Police officer Mohammad Norazzuan Ahmad Zahari arrested again at the Bukit Kayu Hitam border checkpoint; 43 live pangolins found in the boot of his car.");
+// });
 
-$('#three').on('click', function (){
-	$('.route-legend span').html("<strong>SEPT 2018:</strong><br> A police officer arrested with 81 live pangolins in a house rented under his name. The trial is ongoing.");
-});
+// $('#three').on('click', function (){
+// 	$('.route-legend span').html("<strong>SEPT 2018:</strong><br> A police officer arrested with 81 live pangolins in a house rented under his name. The trial is ongoing.");
+// });
 
-$('#four').on('click', function (){
-	$('.route-legend span').html("<strong>FEB 2019:</strong><br> Police officer Ahmad Nasrul Hafifi Mohamad arrested at Sadao customs checkpoint with 47 pangolins in his car, after passing Malaysian border checks.");
-});
+// $('#four').on('click', function (){
+// 	$('.route-legend span').html("<strong>FEB 2019:</strong><br> Police officer Ahmad Nasrul Hafifi Mohamad arrested at Sadao customs checkpoint with 47 pangolins in his car, after passing Malaysian border checks.");
+// });
 
-$('#five').on('click', function (){
-	$('.route-legend span').html("All three officers worked at the Kedah state police headquarters (IPK Kedah). Investigations suggest that there is a larger smuggling ring run by members of the police and officers from other enforcement agencies.");
-});
+// $('#five').on('click', function (){
+// 	$('.route-legend span').html("All three officers worked at the Kedah state police headquarters (IPK Kedah). Investigations suggest that there is a larger smuggling ring run by members of the police and officers from other enforcement agencies.");
+// });
 
 
 
