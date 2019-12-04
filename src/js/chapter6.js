53167b74f7af122d790c7fc16bedf11bd1beaf14
richasyal <richasyal@gmail.com>
2019-12-04 11:58:43 +0800

    replaced policemand svg, added button folders

--- src/js/chapter6.js
+++ src/js/chapter6.js
@@ -19,37 +19,6 @@ $(".close").on("click", function(e) {
 });
 
 
-// tap to find mobile legend
-
-// $('#one').on('click', function (){
-// 	$('.route-legend span').html("<strong>Xiangzhou Port</strong><br>The smugglers embarked from this port on a wooden fishing vessel");
-// });
-
-// $('#two').on('click', function (){
-// 	$('.route-legend span').html("<strong>First pick-up point</strong><br>The smugglers met with a trawler at this point. The crew transferred a batch of pangolins from the trawler to their boat");
-// });
-
-// $('#three').on('click', function (){
-// 	$('.route-legend span').html("<strong>Second pick-up point</strong><br>Another batch of pangolins transferred from a trawler here. This time, the pangolins were in white plastic packages.");
-// });
-
-// $('#four').on('click', function (){
-// 	$('.route-legend span').html("<strong>Off the coast of Zhuhai International Airport</strong><br>The final destination of the smugglers. Here they were arrested by customs officers, who received a tip-off about the shipment.");
-// });
-
-// $('#five').on('click', function (){
-// 	$('.route-legend span').html("<strong>Feb 2019 seizure: Factory</strong><br>Location where pangolins were allegedly processed. One man was arrested at this location.");
-// });
-
-// $('#six').on('click', function (){
-// 	$('.route-legend span').html("<strong>Feb 2019 seizure: WAREHOUSE</strong><br>Location where processed pangolins were found stored in large freezers.");
-// });
-
-// $('#seven').on('click', function (){
-// 	$('.route-legend span').html("<strong>Sepanggar Bay Port</strong><br>Sabah’s only international sea port.");
-// });
-
-
 // desktop popup
 
 $("#_1").click(function () {
