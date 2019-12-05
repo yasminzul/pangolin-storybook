f2408e05ce2d96fb2c67bb434cf3afbb4288b8bd
yasminzul <ainayasminmpm@gmail.com>
2019-12-05 14:05:22 +0800

    removing animsition complete

--- src/js/chapter.js
+++ src/js/chapter.js
@@ -3,12 +3,11 @@ import '../css/font.css';
 import css from '../css/chapter.css';
 var $ = require("jquery");
 
-// swiper.js
-
-   var swiper = new Swiper('.swiper-container', {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-
+// // swiper.js
+//
+//    var swiper = new Swiper('.swiper-container', {
+//       navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//     });
