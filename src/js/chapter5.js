ab8d74266e5d57f5204a4ea51882bc29a85eecf8
yasminzul <ainayasminmpm@gmail.com>
2019-12-06 16:42:31 +0800

    trying window height thru js

--- src/js/chapter5.js
+++ src/js/chapter5.js
@@ -6,10 +6,17 @@ import '../css/swiper.min.css';
 import Swiper from './../js/plugin/swiper.min.js';
 var $ = require("jquery");
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+var winHeight = $(window).height();
+
+$('body').css({
+  'height' : winHeight,
+});
+
+$(window).resize(function (){
+  $('body').css({
+    'height' : winHeight,
+  });
+});
 
 $("#open-trail").on("click", function() {
   $('.popup').css('display','block');
