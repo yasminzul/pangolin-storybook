d7e0a511a14c93e44e6684e75a8bb94e0367a597
richasyal <richasyal@gmail.com>
2019-12-07 11:27:34 +0800

    setting up portrait modes

--- src/js/aboutproject.js
+++ src/js/aboutproject.js
@@ -1,3 +1,24 @@
 import _ from 'lodash';
 import css from '../css/aboutproject.css';
 var $ = require("jquery");
+
+
+// plain text button toggle
+
+$(".plain-text").on("click", function(e) {
+  $('.landscape').addClass('hide');
+  $('.full-orientation').removeClass('active');
+  $('.full-orientation').addClass('hide');
+  $('.portrait').addClass('active');
+  $('.portrait').removeClass('hide');
+});
+
+$(window).on("orientationchange", function(){
+  $('.landscape').removeClass('hide');
+  $('.landscape').addClass('active');
+  $('.portrait').removeClass('active');
+  $('.portrait').addClass('hide');
+  $('.full-orientation').removeClass('hide');
+  $('.full-orientation').addClass('active');
+  
+});
