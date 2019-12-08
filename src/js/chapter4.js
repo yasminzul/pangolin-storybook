ac8106560e795211c59a23ee82ffba27a68ef4f1
richasyal <richasyal@gmail.com>
2019-12-08 12:56:52 +0800

    changes

--- src/js/chapter4.js
+++ src/js/chapter4.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import '../css/font.css';
-import css from '../css/chapter4.css';
 import '../css/chapter.css';
+import css from '../css/chapter4.css';
 import '../css/soundcite.min.css';
 import './../js/plugin/soundcite.min.js';
 var $ = require("jquery");
