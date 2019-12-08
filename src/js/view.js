3028f60ae5cba462221491ff17c05cf1f0a4b06f
Yasmin Zulhaime <32533400+yasminzul@users.noreply.github.com>
2019-12-08 18:41:06 +0800

    adding list of chapters in burger menu

--- src/js/view.js
+++ src/js/view.js
@@ -38,6 +38,31 @@ let menu = `<div id=burger-icon><span></span><span></span><span></span><span></s
   <td><img src=menu/share@3x.png></td>
 </tr>
 </table>
+<ul>
+<a href=index.html><li>Home</li></a>
+<a href=about-pangolin.html><li>About The Pangolins</li></a>
+<li>All chapters
+  <ul class=menu-chapter>
+    <a href=chapter-1.html><li>Chapter 1 : The Middleman</li></a>
+    <a href=chapter-2.html><li>Chapter 2 : The Boatman</li></a>
+    <a href=chapter-3.html><li>Chapter 3 : The Smuggler</li></a>
+    <a href=chapter-4.html><li>Chapter 4 : Follow The Trail Of A Smuggled Pangolin in Malaysia</li></a>
+    <a href=chapter-5.html><li>Chapter 5 : The Policeman</li></a>
+    <a href=chapter-6.html><li>Chapter 6 : The Waiting Wife</li></a>
+    <a href=chapter-8.html><li>Chapter 7 : The Secret Pangolin Smuggling Routes Around The World</li></a>
+    <a href=chapter-9.html><li>Chapter 8 : The Social Media Star</li></a>
+    <a href=chapter-10.html><li>Chapter 9 : The Authorities</li></a>
+    <a href=hapter-11.html><li>Chapter 10 : The Two Doctors</li></a>
+    <a href=chapter-12.html><li>Chapter 11 : The No-Pangolin Recipes</li></a>
+    <a href=chapter-13.html><li>Chapter 12 : The Hunters</li></a>
+    <a href=chapter-14.html><li>Chapter 13 : The Forest</li></a>
+    <a href=chapter-14.html><li>Rainie’s Song</li></a>
+  </ul>
+</li>
+  <a href=quiz.html><li>Quiz: Which Pangolin Protector Are You?</li></a>
+  <a href=https://www.rage.com.my/><li>About this project</li></a>
+  <a href=https://www.rage.com.my/><li>The Pangolin Reports: Trafficked to Extenction</li></a>
+</ul>
 </div>`;
 
 
